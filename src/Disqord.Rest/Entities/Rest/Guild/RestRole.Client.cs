976c1c0535415b2770f91d3086b73bd96c76dc40
Daniel Jarski <danieljarski@gmail.com>
2019-10-19 02:12:43 -0500

    Separated role entity client methods from properties...
    ...to bring them in line with other entities.

---
+++ src/Disqord.Rest/Entities/Rest/Guild/RestRole.Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Disqord.Rest
+{
+    public sealed partial class RestRole : RestSnowflakeEntity, IRole
+    {
+        public async Task ModifyAsync(Action<ModifyRoleProperties> action, RestRequestOptions options = null)
+        {
+            var model = await Client.InternalModifyRoleAsync(GuildId, Id, action, options).ConfigureAwait(false);
+            Update(model);
+        }
+
+        public Task DeleteAsync(RestRequestOptions options = null)
+            => Client.DeleteRoleAsync(GuildId, Id, options);
+    }
+}
