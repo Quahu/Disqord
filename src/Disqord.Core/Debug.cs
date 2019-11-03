2f6145945c9129009da32601d2f115d59bf420fe
Quahu <quahuu@gmail.com>
2019-11-03 12:10:53 +0100

    Added debug JSON dump blocks.

--- src/Disqord.Core/Debug.cs
+++ src/Disqord.Core/Debug.cs
@@ -1,12 +1,27 @@
 ﻿#if DEBUG
+using System;
 using System.ComponentModel;
 
 namespace Disqord
 {
     [EditorBrowsable(EditorBrowsableState.Never)]
-    public static class Debug
+    internal static class Debug
     {
         public static bool DumpJson;
+
+        public static IDisposable DumpingJson()
+            => new DumpingJsonBlock();
+
+        private sealed class DumpingJsonBlock : IDisposable
+        {
+            public DumpingJsonBlock()
+            {
+                DumpJson = true;
+            }
+
+            public void Dispose()
+                => DumpJson = false;
+        }
     }
 }
 #endif
