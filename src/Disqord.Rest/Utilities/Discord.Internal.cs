7c1c467b1012691d322b860e75f40e6f8cea6b3b
Quahu <quahuu@gmail.com>
2019-11-04 12:54:54 +0100

    Moved Discord.Internal to another file.

---
+++ src/Disqord.Rest/Utilities/Discord.Internal.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using System.Runtime.CompilerServices;
+using Disqord.Rest;
+
+namespace Disqord
+{
+    public static partial class Discord
+    {
+        internal static class Internal
+        {
+            internal static string GetAvatarUrl(RestWebhook webhook, ImageFormat format = default, int size = 2048)
+                => webhook.AvatarHash != null
+                    ? GetUserAvatarUrl(webhook.Id, webhook.AvatarHash, format, size)
+                    : GetDefaultUserAvatarUrl(DefaultAvatarColor.Blurple);
+
+            internal static string GetAvatarUrl(IUser user, ImageFormat format = default, int size = 2048)
+                => user.AvatarHash != null
+                    ? GetUserAvatarUrl(user.Id, user.AvatarHash, format, size)
+                    : GetDefaultUserAvatarUrl(user.Discriminator);
+
+            internal static CultureInfo CreateLocale(string locale)
+                => CultureInfo.ReadOnly(new CultureInfo(locale));
+        }
+    }
+}
--- src/Disqord.Rest/Utilities/Discord.cs
+++ src/Disqord.Rest/Utilities/Discord.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Text.RegularExpressions;
-using Disqord.Rest;
 
 namespace Disqord
 {
@@ -11,22 +9,6 @@ namespace Disqord
     /// </summary>
     public static partial class Discord
     {
-        internal static class Internal
-        {
-            internal static string GetAvatarUrl(RestWebhook webhook, ImageFormat format = default, int size = 2048)
-                => webhook.AvatarHash != null
-                    ? GetUserAvatarUrl(webhook.Id, webhook.AvatarHash, format, size)
-                    : GetDefaultUserAvatarUrl(DefaultAvatarColor.Blurple);
-
-            internal static string GetAvatarUrl(IUser user, ImageFormat format = default, int size = 2048)
-                => user.AvatarHash != null
-                    ? GetUserAvatarUrl(user.Id, user.AvatarHash, format, size)
-                    : GetDefaultUserAvatarUrl(user.Discriminator);
-
-            internal static CultureInfo CreateLocale(string locale)
-                => CultureInfo.ReadOnly(new CultureInfo(locale));
-        }
-
         public const int DEFAULT_MAX_PRESENCE_COUNT = 5000;
 
         /// <summary>
