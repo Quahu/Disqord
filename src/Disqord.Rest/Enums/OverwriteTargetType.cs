3051b12312337d8c388c745ce92b1c284dfe85f7
Quahu <quahuu@gmail.com>
2019-11-06 14:07:59 +0100

    Marked OverwriteTargetType as a StringEnum.

--- src/Disqord.Rest/Enums/OverwriteTargetType.cs
+++ src/Disqord.Rest/Enums/OverwriteTargetType.cs
@@ -1,9 +1,15 @@
-﻿namespace Disqord
+﻿using System.Runtime.Serialization;
+using Disqord.Serialization.Json;
+
+namespace Disqord
 {
+    [StringEnum]
     public enum OverwriteTargetType : byte
     {
+        [EnumMember(Value = "role")]
         Role,
 
+        [EnumMember(Value = "member")]
         Member
     }
 }
