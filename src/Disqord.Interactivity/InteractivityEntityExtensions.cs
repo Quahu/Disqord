2c3842a2f73823b3157e991dfd959001e744d394
Quahu <quahuu@gmail.com>
2019-11-11 03:03:50 +0100

    Implemented basic interactivity.

---
+++ src/Disqord.Interactivity/InteractivityEntityExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Disqord.Events;
+
+namespace Disqord.Interactivity
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static class InteractivityEntityExtensions
+    {
+        public static Task<MessageReceivedEventArgs> WaitForMessageAsync(this ICachedMessageChannel channel, Predicate<MessageReceivedEventArgs> predicate, TimeSpan timeout = default)
+        {
+            var extension = GetExtension(channel.Client);
+            return extension.WaitForMessageAsync(predicate, timeout);
+        }
+
+        public static Task<ReactionAddedEventArgs> WaitForReactionAsync(this ICachedMessageChannel channel, Predicate<ReactionAddedEventArgs> predicate, TimeSpan timeout = default)
+        {
+            var extension = GetExtension(channel.Client);
+            return extension.WaitForReactionAsync(predicate, timeout);
+        }
+
+        private static InteractivityExtension GetExtension(DiscordClientBase client)
+        {
+            var extension = client.GetExtension<InteractivityExtension>();
+            if (extension == null)
+                throw new InvalidOperationException("This client does not have the interactivity extension added.");
+
+            return extension;
+        }
+    }
+}
---
+++ src/Disqord.Interactivity/InteractivityExtension.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading.Tasks;
+using Disqord.Events;
+
+namespace Disqord.Interactivity
+{
+    public sealed class InteractivityExtension : DiscordClientExtension
+    {
+        public static readonly TimeSpan DefaultMessageTimeout = TimeSpan.FromSeconds(15);
+
+        public static readonly TimeSpan DefaultReactionTimeout = TimeSpan.FromSeconds(15);
+
+        public InteractivityExtension()
+        { }
+
+        protected override ValueTask SetupAsync()
+            => default;
+
+        public async Task<MessageReceivedEventArgs> WaitForMessageAsync(Predicate<MessageReceivedEventArgs> predicate, TimeSpan timeout = default)
+        {
+            timeout = timeout == default
+                ? DefaultMessageTimeout
+                : timeout;
+
+            var tcs = new TaskCompletionSource<MessageReceivedEventArgs>();
+            Task CheckAsync(MessageReceivedEventArgs e)
+            {
+                if (predicate(e))
+                    tcs.SetResult(e);
+
+                return Task.CompletedTask;
+            }
+
+            Client.MessageReceived += CheckAsync;
+            var task = tcs.Task;
+            var delay = Task.Delay(timeout);
+            var taskOrDelay = await Task.WhenAny(task, delay).ConfigureAwait(false);
+            Client.MessageReceived -= CheckAsync;
+
+            return taskOrDelay == task
+                ? await task.ConfigureAwait(false)
+                : null;
+        }
+
+        public async Task<ReactionAddedEventArgs> WaitForReactionAsync(Predicate<ReactionAddedEventArgs> predicate, TimeSpan timeout = default)
+        {
+            timeout = timeout == default
+                ? DefaultReactionTimeout
+                : timeout;
+
+            var tcs = new TaskCompletionSource<ReactionAddedEventArgs>();
+            Task CheckAsync(ReactionAddedEventArgs e)
+            {
+                if (predicate(e))
+                    tcs.SetResult(e);
+
+                return Task.CompletedTask;
+            }
+
+            Client.ReactionAdded += CheckAsync;
+            var task = tcs.Task;
+            var delay = Task.Delay(timeout);
+            var taskOrDelay = await Task.WhenAny(task, delay).ConfigureAwait(false);
+            Client.ReactionAdded -= CheckAsync;
+
+            return taskOrDelay == task
+                ? await task.ConfigureAwait(false)
+                : null;
+        }
+    }
+}
