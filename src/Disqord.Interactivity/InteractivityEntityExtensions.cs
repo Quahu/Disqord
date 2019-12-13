77dcae62a55b77c1fb364b48c485e646ff6f2184
Quahu <quahuu@gmail.com>
2019-12-13 05:19:47 +0100

    Adjusted interactivity channel extensions to ignore other channels.

--- src/Disqord.Interactivity/InteractivityEntityExtensions.cs
+++ src/Disqord.Interactivity/InteractivityEntityExtensions.cs
@@ -8,14 +8,30 @@ namespace Disqord.Interactivity
     [EditorBrowsable(EditorBrowsableState.Never)]
     public static class InteractivityEntityExtensions
     {
-        public static Task<MessageReceivedEventArgs> WaitForMessageAsync(this ICachedMessageChannel channel, Predicate<MessageReceivedEventArgs> predicate, TimeSpan timeout = default)
+        public static Task<MessageReceivedEventArgs> WaitForMessageAsync(this ICachedMessageChannel channel, Predicate<MessageReceivedEventArgs> predicate = null, TimeSpan timeout = default)
         {
+            if (channel == null)
+                throw new ArgumentNullException(nameof(channel));
+
+            Predicate<MessageReceivedEventArgs> channelPredicate = e => e.Message.Channel.Id == channel.Id;
+            predicate = predicate == null
+                ? channelPredicate
+                : predicate + channelPredicate;
+
             var extension = GetExtension(channel.Client);
             return extension.WaitForMessageAsync(predicate, timeout);
         }
 
-        public static Task<ReactionAddedEventArgs> WaitForReactionAsync(this ICachedMessageChannel channel, Predicate<ReactionAddedEventArgs> predicate, TimeSpan timeout = default)
+        public static Task<ReactionAddedEventArgs> WaitForReactionAsync(this ICachedMessageChannel channel, Predicate<ReactionAddedEventArgs> predicate = null, TimeSpan timeout = default)
         {
+            if (channel == null)
+                throw new ArgumentNullException(nameof(channel));
+
+            Predicate<ReactionAddedEventArgs> channelPredicate = e => e.Channel.Id == channel.Id;
+            predicate = predicate == null
+                ? channelPredicate
+                : predicate + channelPredicate;
+
             var extension = GetExtension(channel.Client);
             return extension.WaitForReactionAsync(predicate, timeout);
         }
