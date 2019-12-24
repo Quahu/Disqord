3aced8290e8294e8f3d11b3ee8a25f215bdcfe3d
Quahu <quahuu@gmail.com>
2019-12-24 19:52:34 +0100

    Added PrivateOnlyAttribute.

---
+++ src/Disqord.Bot/Commands/Checks/PrivateOnlyAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Qmmands;
+
+namespace Disqord.Bot
+{
+    public sealed class PrivateOnlyAttribute : CheckAttribute
+    {
+        public PrivateOnlyAttribute()
+        { }
+
+        public override ValueTask<CheckResult> CheckAsync(CommandContext _)
+        {
+            var context = _ as DiscordCommandContext;
+            return context.Guild == null
+                ? CheckResult.Successful
+                : CheckResult.Unsuccessful("This can only be executed in a private channel.");
+        }
+    }
+}
