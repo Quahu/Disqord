23b85f092e011cd9054b7e27b7568dc5a3f2e310
Quahu <quahuu@gmail.com>
2019-12-30 21:10:05 +0100

    Updated GUILD_DELETE handling to account for somehow uncached guilds.

--- src/Disqord/Client/Gateway/State/Events/GuildDelete.cs
+++ src/Disqord/Client/Gateway/State/Events/GuildDelete.cs
@@ -12,14 +12,25 @@ namespace Disqord
             var model = Serializer.ToObject<WebSocketGuildModel>(payload.D);
             if (model.Unavailable.HasValue)
             {
-                _guilds.TryGetValue(model.Id, out var guild);
+                if (!_guilds.TryGetValue(model.Id, out var guild))
+                {
+                    Log(LogMessageSeverity.Information, $"Guild {model.Id} is uncached and became unavailable.");
+                    return Task.CompletedTask;
+                }
+
                 // TODO set unavailable or something
                 Log(LogMessageSeverity.Information, $"Guild '{guild}' ({guild.Id}) became unavailable.");
                 return _client._guildUnavailable.InvokeAsync(new GuildUnavailableEventArgs(guild));
             }
             else
             {
-                _guilds.TryRemove(model.Id, out var guild);
+                if (!_guilds.TryRemove(model.Id, out var guild))
+                {
+                    // This should only ever be the case for user tokens and lurkable guilds?
+                    Log(LogMessageSeverity.Information, $"Left uncached guild {model.Id}.");
+                    return Task.CompletedTask;
+                }
+
                 foreach (var member in guild.Members.Values)
                     member.SharedUser.References--;
 
