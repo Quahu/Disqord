0561c35938273914ed07dda5e86365ed5f955a23
Quahu <quahuu@gmail.com>
2020-01-24 09:27:02 +0100

    Cleaned up RestRequestEnumerator, added searching utility methods.

--- src/Disqord.Rest/Client/Requests/Pagination/RestRequestEnumerable.cs
+++ src/Disqord.Rest/Client/Requests/Pagination/RestRequestEnumerable.cs
@@ -12,6 +12,7 @@ namespace Disqord.Rest
     /// </summary>
     /// <typeparam name="T"> The <see cref="Type"/> of items in pages. </typeparam>
     public sealed class RestRequestEnumerable<T> : IAsyncEnumerable<IReadOnlyList<T>>
+        where T : class
     {
         private readonly RestRequestEnumerator<T> _enumerator;
 
@@ -20,75 +21,51 @@ namespace Disqord.Rest
             _enumerator = enumerator;
         }
 
-        public ConfiguredRestRequestEnumerable WithOptions(RestRequestOptions options)
-            => new ConfiguredRestRequestEnumerable(this, options);
-
         /// <summary>
         ///     Flattens this <see cref="RestRequestEnumerable{T}"/> into a single read-only list.
         /// </summary>
-        /// <param name="options"> The <see cref="RestRequestOptions"/> to use for requests. </param>
         /// <returns>
         ///     A flattened list of <typeparamref name="T"/>s.
         /// </returns>
-        public Task<IReadOnlyList<T>> FlattenAsync(RestRequestOptions options = null)
-            => FlattenAsync(CancellationToken.None, options);
-
-        /// <summary>
-        ///     Flattens this <see cref="RestRequestEnumerable{T}"/> into a single read-only list.
-        /// </summary>
-        /// <param name="cancellationToken"> The <see cref="CancellationToken"/> to use for requests. </param>
-        /// <param name="options"> The <see cref="RestRequestOptions"/> to use for requests. </param>
-        /// <returns>
-        ///     A flattened list of <typeparamref name="T"/>s.
-        /// </returns>
-        public async Task<IReadOnlyList<T>> FlattenAsync(CancellationToken cancellationToken, RestRequestOptions options = null)
+        public async Task<IReadOnlyList<T>> FlattenAsync()
         {
             var builder = ImmutableArray.CreateBuilder<T>();
-            await foreach (var items in this.WithOptions(options).WithCancellation(cancellationToken).ConfigureAwait(false))
+            await foreach (var items in this.ConfigureAwait(false))
                 builder.AddRange(items);
 
             return builder.ToImmutable();
         }
 
-        public RestRequestEnumerator<T> GetAsyncEnumerator(CancellationToken cancellationToken = default)
+        public async Task<T> FirstOrDefaultAsync(Predicate<T> predicate)
+        {
+            await foreach (var items in this.ConfigureAwait(false))
+            {
+                for (var i = 0; i < items.Count; i++)
+                {
+                    var item = items[i];
+                    if (predicate(item))
+                        return item;
+                }
+            }
+
+            return null;
+        }
+
+        public async Task<IReadOnlyList<T>> FirstOrDefaultPageAsync(Predicate<IReadOnlyList<T>> predicate)
+        {
+            await foreach (var items in this.ConfigureAwait(false))
+            {
+                if (predicate(items))
+                    return items;
+            }
+
+            return null;
+        }
+
+        public RestRequestEnumerator<T> GetAsyncEnumerator()
             => _enumerator;
 
         IAsyncEnumerator<IReadOnlyList<T>> IAsyncEnumerable<IReadOnlyList<T>>.GetAsyncEnumerator(CancellationToken cancellationToken)
             => _enumerator;
-
-        public readonly struct ConfiguredRestRequestEnumerable : IAsyncEnumerable<IReadOnlyList<T>>
-        {
-            private readonly RestRequestEnumerable<T> _enumerable;
-            private readonly RestRequestOptions _options;
-
-            internal ConfiguredRestRequestEnumerable(RestRequestEnumerable<T> enumerable, RestRequestOptions options = null)
-            {
-                _enumerable = enumerable;
-                _options = options;
-            }
-
-            public IAsyncEnumerator<IReadOnlyList<T>> GetAsyncEnumerator(CancellationToken cancellationToken = default)
-                => new Enumerator(_enumerable.GetAsyncEnumerator(cancellationToken), _options);
-
-            public readonly struct Enumerator : IAsyncEnumerator<IReadOnlyList<T>>
-            {
-                public IReadOnlyList<T> Current => _enumerator.Current;
-
-                private readonly RestRequestEnumerator<T> _enumerator;
-                private readonly RestRequestOptions _options;
-
-                internal Enumerator(RestRequestEnumerator<T> enumerator, RestRequestOptions options = null)
-                {
-                    _enumerator = enumerator;
-                    _options = options;
-                }
-
-                public ValueTask<bool> MoveNextAsync()
-                    => _enumerator.MoveNextAsync(_options);
-
-                public ValueTask DisposeAsync()
-                    => _enumerator.DisposeAsync();
-            }
-        }
     }
 }
