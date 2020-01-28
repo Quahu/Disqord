c10c7dfc11d94146819bb5d3d986a2393d8f6256
Quahu <quahuu@gmail.com>
2020-01-28 13:43:06 +0100

    Renamed PageProvider to DefaultPageProvider and made it use ReadOnlyList.

--- src/Disqord.Extensions.Interactivity/Menus/Paged/Provider/DefaultPageProvider.cs
+++ src/Disqord.Extensions.Interactivity/Menus/Paged/Provider/DefaultPageProvider.cs
@@ -1,33 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Threading.Tasks;
+using Disqord.Collections;
 
 namespace Disqord.Extensions.Interactivity.Menus.Paged
 {
     /// <summary>
     ///     Provides <see cref="Page"/>s from a collection.
     /// </summary>
-    public sealed class PageProvider : IPageProvider
+    public sealed class DefaultPageProvider : IPageProvider
     {
         /// <summary>
         ///     Gets the collection of <see cref="Page"/>s.
         /// </summary>
-        public ImmutableArray<Page> Pages { get; }
+        public IReadOnlyList<Page> Pages { get; }
 
         /// <inheritdoc/>
-        public int PageCount => Pages.Length;
+        public int PageCount => Pages.Count;
 
         /// <summary>
-        ///     Instantiates a new <see cref="PageProvider"/> with the specified collection of <see cref="Page"/>s.
+        ///     Instantiates a new <see cref="DefaultPageProvider"/> with the specified collection of <see cref="Page"/>s.
         /// </summary>
         /// <param name="pages"> The collection of <see cref="Page"/>s. </param>
-        public PageProvider(IEnumerable<Page> pages)
+        public DefaultPageProvider(IEnumerable<Page> pages)
         {
             if (pages == null)
                 throw new ArgumentNullException(nameof(pages));
 
-            Pages = pages.ToImmutableArray();
+            Pages = pages.ToReadOnlyList();
         }
 
         /// <inheritdoc/>
