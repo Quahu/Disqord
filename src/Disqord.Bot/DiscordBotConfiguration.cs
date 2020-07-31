ce1f29964b51238a94e182e90e5134f4c8f41de3
Quahu <quahuu@gmail.com>
2020-07-31 22:32:14 +0200

    Added the empty ctor to DiscordBotConfiguration.

--- src/Disqord.Bot/DiscordBotConfiguration.cs
+++ src/Disqord.Bot/DiscordBotConfiguration.cs
@@ -7,7 +7,14 @@ namespace Disqord
     public class DiscordBotConfiguration : DiscordClientConfiguration, IDiscordBotBaseConfiguration
     {
         public CommandServiceConfiguration CommandServiceConfiguration { get; set; }
+
         public Func<DiscordBotBase, IServiceProvider> ProviderFactory { get; set; }
+
+        /// <summary>
+        ///     Instantiates a new <see cref="DiscordBotConfiguration"/>.
+        /// </summary>
+        public DiscordBotConfiguration()
+        { }
 
         public static new DiscordBotConfiguration Default => new DiscordBotConfiguration();
     }
