65161c79d4c550cdb5134695fa5a883aa2dc5c19
Quahu <quahuu@gmail.com>
2021-02-19 17:37:48 +0100

    Added RequireUserAttribute.

---
+++ src/Disqord.Bot/Commands/Checks/RequireUserAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Qmmands;
+
+namespace Disqord.Bot
+{
+    public class RequireUserAttribute : CheckAttribute
+    {
+        public Snowflake Id { get; }
+
+        public RequireUserAttribute(ulong id)
+        {
+            Id = id;
+        }
+
+        public override ValueTask<CheckResult> CheckAsync(CommandContext _)
+        {
+            var context = _ as DiscordCommandContext;
+
+            if (context.User.Id == Id)
+                return Success();
+
+            return Failure($"This can only be executed by the user with the ID {Id}.");
+        }
+    }
+}
