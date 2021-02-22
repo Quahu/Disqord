40844184893295c2ab9711e4df7e9504f10e0f82
Quahu <quahuu@gmail.com>
2021-02-22 06:18:24 +0100

    Added preset TryGetCache() extension methods.

---
+++ src/Disqord.Gateway/GatewayCacheProviderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Disqord.Collections.Synchronized;
+
+namespace Disqord.Gateway
+{
+    public static class GatewayCacheProviderExtensions
+    {
+        public static bool TryGetUsers(this IGatewayCacheProvider cacheProvider, out ISynchronizedDictionary<Snowflake, CachedSharedUser> cache)
+            => cacheProvider.TryGetCache(out cache);
+
+        public static bool TryGetGuilds(this IGatewayCacheProvider cacheProvider, out ISynchronizedDictionary<Snowflake, CachedGuild> cache)
+            => cacheProvider.TryGetCache(out cache);
+
+        public static bool TryGetChannels(this IGatewayCacheProvider cacheProvider, Snowflake guildId, out ISynchronizedDictionary<Snowflake, CachedGuildChannel> cache)
+            => cacheProvider.TryGetCache(guildId, out cache);
+
+        public static bool TryGetMembers(this IGatewayCacheProvider cacheProvider, Snowflake guildId, out ISynchronizedDictionary<Snowflake, CachedMember> cache)
+            => cacheProvider.TryGetCache(guildId, out cache);
+    }
+}
