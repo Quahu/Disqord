4b1d0cde02ceb234bc5df5107bd625c1110a7067
Quahu <quahuu@gmail.com>
2021-02-24 06:51:24 +0100

    Added IPrefixProvider and IPrefix.

---
+++ src/Disqord.Bot/Prefixes/Prefixes/IPrefix.cs
@@ -0,0 +1,20 @@
+﻿using Disqord.Gateway;
+
+namespace Disqord.Bot
+{
+    /// <summary>
+    ///     Represents a message prefix used for command execution.
+    /// </summary>
+    public interface IPrefix
+    {
+        /// <summary>
+        ///     Attempts to find this prefix in the specified message and return the substring ready for execution.
+        /// </summary>
+        /// <param name="message"> The message to check for the prefix in. </param>
+        /// <param name="output"> The output substring ready for execution. </param>
+        /// <returns>
+        ///     <see langword="true"/>, if the prefix was found.
+        /// </returns>
+        bool TryFind(IGatewayUserMessage message, out string output);
+    }
+}
---
+++ src/Disqord.Bot/Prefixes/Provider/IPrefixProvider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Disqord.Gateway;
+
+namespace Disqord.Bot
+{
+    /// <summary>
+    ///     Represents a type that that provides prefixes based on the received message.
+    /// </summary>
+    public interface IPrefixProvider
+    {
+        /// <summary>
+        ///     Gets prefixes for the given message.
+        /// </summary>
+        /// <param name="message"> The message to get prefixes for. </param>
+        /// <returns>
+        ///     A <see cref="ValueTask{TResult}"/> wrapping an enumerable of prefixes.
+        /// </returns>
+        ValueTask<IEnumerable<IPrefix>> GetPrefixesAsync(IGatewayUserMessage message);
+    }
+}
