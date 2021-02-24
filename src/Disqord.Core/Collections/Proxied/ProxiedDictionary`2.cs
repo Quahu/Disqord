6b8d5a5bab8945bea2ea81e5c38f217c0294017f
Quahu <quahuu@gmail.com>
2021-02-24 09:54:31 +0100

    [ci skip] Documented ProxiedDictionary`2.

--- src/Disqord.Core/Collections/Proxied/ProxiedDictionary`2.cs
+++ src/Disqord.Core/Collections/Proxied/ProxiedDictionary`2.cs
@@ -3,16 +3,27 @@ using System.Collections.Generic;
 
 namespace Disqord.Collections.Proxied
 {
+    /// <summary>
+    ///     Represents an implementation of <see cref="IDictionary{TKey, TValue}"/> and <see cref="IReadOnlyDictionary{TKey, TValue}"/>
+    ///     by wrapping an <see cref="IDictionary{TKey, TValue}"/> with virtual implementations making overriding individual members very simple.
+    /// </summary>
+    /// <typeparam name="TKey"> The type of keys in the dictionary. </typeparam>
+    /// <typeparam name="TValue"> The type of values in the dictionary. </typeparam>
     public abstract class ProxiedDictionary<TKey, TValue> : IDictionary<TKey, TValue>, IReadOnlyDictionary<TKey, TValue>
     {
+        /// <inheritdoc/>
         public virtual ICollection<TKey> Keys => Dictionary.Keys;
 
+        /// <inheritdoc/>
         public virtual ICollection<TValue> Values => Dictionary.Values;
 
+        /// <inheritdoc/>
         public virtual int Count => Dictionary.Count;
 
+        /// <inheritdoc/>
         public virtual bool IsReadOnly => Dictionary.IsReadOnly;
 
+        /// <inheritdoc/>
         public virtual TValue this[TKey key]
         {
             get => Dictionary[key];
@@ -24,29 +35,40 @@ namespace Disqord.Collections.Proxied
 
         protected IDictionary<TKey, TValue> Dictionary { get; }
 
+        /// <summary>
+        ///     Instantiates a new <see cref="ProxiedDictionary{TKey, TValue}"/>.
+        /// </summary>
+        /// <param name="dictionary"> The dictionary to wrap. </param>
         protected ProxiedDictionary(IDictionary<TKey, TValue> dictionary)
         {
             Dictionary = dictionary;
         }
 
+        /// <inheritdoc/>
         public virtual void Add(TKey key, TValue value)
             => Dictionary.Add(key, value);
 
+        /// <inheritdoc/>
         public virtual void Clear()
             => Dictionary.Clear();
 
+        /// <inheritdoc/>
         public virtual bool ContainsKey(TKey key)
             => Dictionary.ContainsKey(key);
 
+        /// <inheritdoc/>
         public virtual void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
             => Dictionary.CopyTo(array, arrayIndex);
 
+        /// <inheritdoc/>
         public virtual bool Remove(TKey key)
             => Dictionary.Remove(key);
 
+        /// <inheritdoc/>
         public virtual bool TryGetValue(TKey key, out TValue value)
             => Dictionary.TryGetValue(key, out value);
 
+        /// <inheritdoc/>
         public virtual IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
             => GetEnumerator();
 
