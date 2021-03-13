f15026e83276dc12f2cca8ddb9768056ea49f40c
Quahu <quahuu@gmail.com>
2021-03-13 18:31:19 +0100

    Added Guild and CurrentMember to the guild command context.

--- src/Disqord.Bot/Commands/Contexts/DiscordGuildCommandContext.cs
+++ src/Disqord.Bot/Commands/Contexts/DiscordGuildCommandContext.cs
@@ -8,6 +8,32 @@ namespace Disqord.Bot
     {
         public virtual new Snowflake GuildId => base.GuildId.Value;
 
+        public virtual CachedGuild Guild
+        {
+            get
+            {
+                if (_guild == null)
+                    _guild = Bot.GetGuild(GuildId);
+
+                return _guild;
+            }
+            protected set => _guild = value;
+        }
+        private CachedGuild _guild;
+
+        public virtual CachedMember CurrentMember
+        {
+            get
+            {
+                if (_currentMember == null)
+                    _currentMember = Bot.GetMember(GuildId, Bot.CurrentUser.Id);
+
+                return _currentMember;
+            }
+            protected set => _currentMember = value;
+        }
+        private CachedMember _currentMember;
+        
         public virtual new IMember Author => base.Author as IMember;
 
         public virtual CachedTextChannel Channel { get; }
