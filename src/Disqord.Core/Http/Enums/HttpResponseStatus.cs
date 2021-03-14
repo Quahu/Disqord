7ddcc4a896014c786af36cb0dfd879fa5a18443d
Quahu <quahuu@gmail.com>
2021-03-14 13:18:12 +0100

    Added more HTTP response status codes.

--- src/Disqord.Core/Http/Enums/HttpResponseStatus.cs
+++ src/Disqord.Core/Http/Enums/HttpResponseStatus.cs
@@ -2,6 +2,26 @@
 {
     public enum HttpResponseStatusCode : int
     {
-        NotFound = 404
+        Ok = 200,
+
+        Created = 201,
+
+        NoContent = 204,
+
+        NotModified = 304,
+
+        BadRequest = 400,
+
+        Unauthorized = 401,
+
+        Forbidden = 403,
+
+        NotFound = 404,
+
+        MethodNotAllowed = 405,
+
+        TooManyRequests = 429,
+
+        GatewayUnavailable = 502
     }
 }
