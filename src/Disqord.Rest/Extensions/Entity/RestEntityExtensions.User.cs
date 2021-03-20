7f1b18f8138588a238f70f0e2c91bbcea7bb2012
Quahu <quahuu@gmail.com>
2021-03-20 23:01:53 +0100

    Implemented REST user entity extensions.

---
+++ src/Disqord.Rest/Extensions/Entity/RestEntityExtensions.User.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+
+namespace Disqord.Rest
+{
+    public static partial class RestEntityExtensions
+    {
+        public static Task LeaveAsync(this IGuild guild, IRestRequestOptions options = null)
+        {
+            var client = guild.GetRestClient();
+            return client.LeaveGuildAsync(guild.Id, options);
+        }
+
+        public static Task<IDirectChannel> CreateDirectChannelAsync(this IUser user, IRestRequestOptions options = null)
+        {
+            var client = user.GetRestClient();
+            return client.CreateDirectChannelAsync(user.Id, options);
+        }
+    }
+}
