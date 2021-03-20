549e765de95b1a8714ca30301cf55b8bedf9e406
Quahu <quahuu@gmail.com>
2021-03-20 23:42:21 +0100

    Removed FetchGuildAsync from the role extensions.

--- src/Disqord.Rest/Extensions/Entity/RestEntityExtensions.Role.cs
+++ src/Disqord.Rest/Extensions/Entity/RestEntityExtensions.Role.cs
@@ -5,12 +5,6 @@ namespace Disqord.Rest
 {
     public static partial class RestEntityExtensions
     {
-        public static Task<IGuild> FetchGuildAsync(this IRole role, IRestRequestOptions options = null)
-        {
-            var client = role.GetRestClient();
-            return client.FetchGuildAsync(role.GuildId, options: options);
-        }
-
         public static Task<IRole> ModifyAsync(this IRole role, Action<ModifyRoleActionProperties> action, IRestRequestOptions options = null)
         {
             var client = role.GetRestClient();
