ebf70764947f1cd94e14220351dceeae0a1b5acf
Quahu <quahuu@gmail.com>
2021-03-24 01:05:23 +0100

    Split Snowflake into multiple files.

--- src/Disqord.Core/Snowflake.cs
+++ src/Disqord.Core/Snowflake.cs
@@ -2,7 +2,7 @@
 
 namespace Disqord
 {
-    public readonly struct Snowflake : IConvertible, IEquatable<ulong>, IEquatable<Snowflake>, IComparable<ulong>, IComparable<Snowflake>
+    public readonly partial struct Snowflake : IConvertible, IEquatable<ulong>, IEquatable<Snowflake>, IComparable<ulong>, IComparable<Snowflake>
     {
         public const ulong DISCORD_EPOCH = 1420070400000;
 
@@ -27,13 +27,19 @@ namespace Disqord
         public bool Equals(Snowflake other)
             => RawValue == other.RawValue;
 
+        public int CompareTo(ulong other)
+            => RawValue.CompareTo(other);
+
+        public int CompareTo(Snowflake other)
+            => RawValue.CompareTo(other.RawValue);
+
         public override bool Equals(object obj)
         {
-            if (obj is Snowflake snowflake)
-                return snowflake.RawValue == RawValue;
+            if (obj is Snowflake otherSnowflake)
+                return RawValue == otherSnowflake.RawValue;
 
-            if (obj is ulong rawValue)
-                return rawValue == RawValue;
+            if (obj is ulong otherRawValue)
+                return RawValue == otherRawValue;
 
             return false;
         }
@@ -44,30 +50,12 @@ namespace Disqord
         public override string ToString()
             => RawValue.ToString();
 
-        public static bool operator ==(Snowflake left, Snowflake right)
-            => left.RawValue == right.RawValue;
-
-        public static bool operator !=(Snowflake left, Snowflake right)
-            => left.RawValue != right.RawValue;
-
-        public static bool operator <(Snowflake left, Snowflake right)
-            => left.CompareTo(right) < 0;
-
-        public static bool operator <=(Snowflake left, Snowflake right)
-            => left.CompareTo(right) <= 0;
-
-        public static bool operator >(Snowflake left, Snowflake right)
-            => left.CompareTo(right) > 0;
-
-        public static bool operator >=(Snowflake left, Snowflake right)
-            => left.CompareTo(right) >= 0;
-
         public static bool TryParse(string value, out Snowflake result)
             => TryParse(value.AsSpan(), out result);
 
         public static bool TryParse(ReadOnlySpan<char> value, out Snowflake result)
         {
-            if (value.Length >= 15 && value.Length <= 21 && ulong.TryParse(value, out var ulongResult))
+            if (value.Length >= 15 && value.Length < 21 && ulong.TryParse(value, out var ulongResult))
             {
                 result = ulongResult;
                 return true;
@@ -81,7 +69,7 @@ namespace Disqord
             => Parse(value.AsSpan());
 
         public static Snowflake Parse(ReadOnlySpan<char> value)
-            => value.Length >= 15 && value.Length <= 21
+            => value.Length >= 15 && value.Length < 21
                 ? ulong.Parse(value)
                 : throw new FormatException();
 
@@ -91,67 +79,5 @@ namespace Disqord
         public static DateTimeOffset ToDateTimeOffset(ulong id)
             => DateTimeOffset.FromUnixTimeMilliseconds((long) ((id >> 22) + DISCORD_EPOCH));
 
-        public int CompareTo(ulong other)
-            => RawValue.CompareTo(other);
-
-        public int CompareTo(Snowflake other)
-            => RawValue.CompareTo(other);
-
-        TypeCode IConvertible.GetTypeCode()
-            => RawValue.GetTypeCode();
-
-        bool IConvertible.ToBoolean(IFormatProvider provider)
-            => ((IConvertible) RawValue).ToBoolean(provider);
-
-        byte IConvertible.ToByte(IFormatProvider provider)
-            => ((IConvertible) RawValue).ToByte(provider);
-
-        char IConvertible.ToChar(IFormatProvider provider)
-            => ((IConvertible) RawValue).ToChar(provider);
-
-        DateTime IConvertible.ToDateTime(IFormatProvider provider)
-            => ((IConvertible) RawValue).ToDateTime(provider);
-
-        decimal IConvertible.ToDecimal(IFormatProvider provider)
-            => ((IConvertible) RawValue).ToDecimal(provider);
-
-        double IConvertible.ToDouble(IFormatProvider provider)
-            => ((IConvertible) RawValue).ToDouble(provider);
-
-        short IConvertible.ToInt16(IFormatProvider provider)
-            => ((IConvertible) RawValue).ToInt16(provider);
-
-        int IConvertible.ToInt32(IFormatProvider provider)
-            => ((IConvertible) RawValue).ToInt32(provider);
-
-        long IConvertible.ToInt64(IFormatProvider provider)
-            => ((IConvertible) RawValue).ToInt64(provider);
-
-        sbyte IConvertible.ToSByte(IFormatProvider provider)
-            => ((IConvertible) RawValue).ToSByte(provider);
-
-        float IConvertible.ToSingle(IFormatProvider provider)
-            => ((IConvertible) RawValue).ToSingle(provider);
-
-        string IConvertible.ToString(IFormatProvider provider)
-            => RawValue.ToString(provider);
-
-        object IConvertible.ToType(Type conversionType, IFormatProvider provider)
-            => ((IConvertible) RawValue).ToType(conversionType, provider);
-
-        ushort IConvertible.ToUInt16(IFormatProvider provider)
-            => ((IConvertible) RawValue).ToUInt16(provider);
-
-        uint IConvertible.ToUInt32(IFormatProvider provider)
-            => ((IConvertible) RawValue).ToUInt32(provider);
-
-        ulong IConvertible.ToUInt64(IFormatProvider provider)
-            => ((IConvertible) RawValue).ToUInt64(provider);
-
-        public static implicit operator Snowflake(ulong value)
-            => new Snowflake(value);
-
-        public static implicit operator ulong(Snowflake value)
-            => value.RawValue;
     }
 }
