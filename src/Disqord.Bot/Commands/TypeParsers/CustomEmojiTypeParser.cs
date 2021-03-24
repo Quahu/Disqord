1978a36b3d8aade0126d6a30c3a20715da471876
Quahu <quahuu@gmail.com>
2021-03-24 02:59:59 +0100

    Added CustomEmojiTypeParser.

---
+++ src/Disqord.Bot/Commands/TypeParsers/CustomEmojiTypeParser.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Qmmands;
+
+namespace Disqord.Bot.Parsers
+{
+    public class CustomEmojiTypeParser : DiscordTypeParser<ICustomEmoji>
+    {
+        public override ValueTask<TypeParserResult<ICustomEmoji>> ParseAsync(Parameter parameter, string value, DiscordCommandContext context)
+        {
+            if (LocalCustomEmoji.TryParse(value, out var emoji))
+                return Success(emoji);
+
+            return Failure("Invalid custom emoji.");
+        }
+    }
+}
