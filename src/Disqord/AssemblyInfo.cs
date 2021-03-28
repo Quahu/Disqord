9dcdfb2c832b8798317bc4a103617c8a80912a5c
Quahu <quahuu@gmail.com>
2021-03-28 02:44:11 +0100

    Updated AssemblyInfo for the Disqord project.

--- src/Disqord/AssemblyInfo.cs
+++ src/Disqord/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Disqord.Extensions.Interactivity")]
 [assembly: InternalsVisibleTo("Disqord.Extensions.Slash")]
 [assembly: InternalsVisibleTo("Disqord.Extensions.Voice")]
