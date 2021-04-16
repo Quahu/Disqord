5f827e6f92e59e14942fb18a2beaf5abbc8dc9ef
Quahu <quahuu@gmail.com>
2021-04-16 11:33:08 +0200

    Added RequireGuildOwnerAttribute.

---
+++ src/Disqord.Bot/Commands/Checks/RequireGuildOwnerAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Qmmands;
+
+namespace Disqord.Bot
+{
+    /// <summary>
+    ///     Specifies that the module or command can only be executed by the guild owner.
+    /// </summary>
+    public class RequireGuildOwnerAttribute : DiscordGuildCheckAttribute
+    {
+        public override ValueTask<CheckResult> CheckAsync(DiscordGuildCommandContext context)
+        {
+            if (context.Author.Id == context.Guild.OwnerId)
+                return Success();
+
+            return Failure("This can only be executed by the guild owner.");
+        }
+    }
+}
