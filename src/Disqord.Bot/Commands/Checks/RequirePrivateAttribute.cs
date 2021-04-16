94937463249d4ab6099a3e8cf83783b36a989070
Quahu <quahuu@gmail.com>
2021-04-16 11:33:27 +0200

    Added RequirePrivateAttribute.

---
+++ src/Disqord.Bot/Commands/Checks/RequirePrivateAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Qmmands;
+
+namespace Disqord.Bot
+{
+    public class RequirePrivateAttribute : DiscordCheckAttribute
+    {
+        public RequirePrivateAttribute()
+        { }
+
+        public override sealed ValueTask<CheckResult> CheckAsync(DiscordCommandContext context)
+        {
+            if (context.GuildId == null)
+                return Success();
+
+            return Failure("This can only be executed outside of a guild.");
+        }
+    }
+}
