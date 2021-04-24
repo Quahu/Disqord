bd2adf135526a6dc9bb4e4f09468c7e7319dc4d8
Quahu <quahuu@gmail.com>
2021-04-24 22:06:44 +0200

    Made the prefix provider use an immutable set.

--- src/Disqord.Bot/Prefixes/Provider/Default/DefaultPrefixProvider.cs
+++ src/Disqord.Bot/Prefixes/Provider/Default/DefaultPrefixProvider.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Threading.Tasks;
-using Disqord.Collections;
-using Disqord.Collections.Synchronized;
 using Disqord.Gateway;
 using Microsoft.Extensions.Options;
 
@@ -9,17 +8,31 @@ namespace Disqord.Bot
 {
     /// <summary>
     ///     Represents the default implementation of an <see cref="IPrefixProvider"/>.
-    ///     Returns the same set of prefixes for all messages and is not mutable by default.
+    ///     Returns <see cref="Prefixes"/> for all messages.
     /// </summary>
     public class DefaultPrefixProvider : IPrefixProvider
     {
         /// <summary>
-        ///     Gets the prefixes set of this <see cref="DefaultPrefixProvider"/>.
+        ///     Gets or sets the immutable set of prefixes of this <see cref="DefaultPrefixProvider"/>.
         /// </summary>
-        /// <remarks>
-        ///     This set is read-only by default, unless specified otherwise by <see cref="DefaultPrefixProviderConfiguration.AllowsMutability"/>.
-        /// </remarks>
-        public ISet<IPrefix> Prefixes { get; }
+        public IImmutableSet<IPrefix> Prefixes
+        {
+            get
+            {
+                lock (this)
+                {
+                    return _prefixes;
+                }
+            }
+            set
+            {
+                lock (this)
+                {
+                    _prefixes = value;
+                }
+            }
+        }
+        private IImmutableSet<IPrefix> _prefixes;
 
         /// <summary>
         ///     Instantiates a new <see cref="DefaultPrefixProvider"/>.
@@ -29,16 +42,7 @@ namespace Disqord.Bot
             IOptions<DefaultPrefixProviderConfiguration> options)
         {
             var configuration = options.Value;
-            var prefixes = configuration.Prefixes ?? new IPrefix[0];
-            if (configuration.AllowsMutability)
-            {
-                // Mutability comes at the price of ToArray()ing the set on every message.
-                Prefixes = new SynchronizedHashSet<IPrefix>(prefixes);
-            }
-            else
-            {
-                Prefixes = new ReadOnlySet<IPrefix>(new HashSet<IPrefix>(prefixes));
-            }
+            _prefixes = configuration.Prefixes?.ToImmutableHashSet() ?? ImmutableHashSet<IPrefix>.Empty;
         }
 
         /// <inheritdoc/>
