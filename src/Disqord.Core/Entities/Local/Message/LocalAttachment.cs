a7c21d3f2b331c2096c44eae28ea74b007237e0e
Quahu <quahuu@gmail.com>
2021-05-31 03:49:29 +0200

    Updated LocalAttachment to match LocalEmoji's style.

--- src/Disqord.Core/Entities/Local/Message/LocalAttachment.cs
+++ src/Disqord.Core/Entities/Local/Message/LocalAttachment.cs
@@ -6,56 +6,46 @@ namespace Disqord
     /// <summary>
     ///     Represents a local attachment to be sent to Discord.
     /// </summary>
-    public class LocalAttachment : IDisposable
+    public class LocalAttachment : ILocalConstruct, IDisposable
     {
         /// <summary>
         ///     The prefix for spoiler attachments.
         /// </summary>
         public const string SPOILER_PREFIX = "SPOILER_";
 
-        /// <summary>
-        ///     Gets or sets the <see cref="System.IO.Stream"/> of this attachment..
-        /// </summary>
-        public Stream Stream { get; }
+        public static LocalAttachment File(string path, string fileName = null, bool isSpoiler = false)
+            => File(System.IO.File.OpenRead(path), fileName, isSpoiler);
+
+        public static LocalAttachment File(FileStream fileStream, string fileName = null, bool isSpoiler = false)
+            => new(fileStream, fileName ?? Path.GetFileName(fileStream.Name), isSpoiler);
+
+        public static LocalAttachment Bytes(ArraySegment<byte> bytes, string fileName, bool isSpoiler = false)
+            => new(new MemoryStream(bytes.Array, bytes.Offset, bytes.Count, true, true), fileName, isSpoiler);
 
         /// <summary>
-        ///     Gets the file name.
+        ///     Gets or sets the <see cref="System.IO.Stream"/> of this attachment.
         /// </summary>
-        public string FileName { get; }
+        public Stream Stream { get; init; }
 
         /// <summary>
-        ///     Gets whether this attachment should be sent as a spoiler or not.
+        ///     Gets or sets the file name of this attachment.
         /// </summary>
-        public bool IsSpoiler { get; }
+        public string FileName { get; init; }
+
+        /// <summary>
+        ///     Gets or sets whether this attachment should be sent as a spoiler or not.
+        /// </summary>
+        public bool IsSpoiler { get; init; }
+
+        public LocalAttachment()
+        { }
 
         public LocalAttachment(Stream stream, string fileName, bool isSpoiler = false)
         {
-            if (stream == null)
-                throw new ArgumentNullException(nameof(stream));
-
-            if (fileName == null)
-                throw new ArgumentNullException(nameof(fileName));
-
             Stream = stream;
             FileName = fileName;
             IsSpoiler = isSpoiler;
         }
-
-        public LocalAttachment(FileStream fileStream, string fileName = null, bool isSpoiler = false)
-            : this(fileStream as Stream, fileName ?? Path.GetFileName(fileStream.Name), isSpoiler)
-        { }
-
-        public LocalAttachment(string path, string fileName = null, bool isSpoiler = false)
-            : this(File.OpenRead(path), fileName, isSpoiler)
-        { }
-
-        public LocalAttachment(byte[] bytes, string fileName, bool isSpoiler = false)
-            : this(new MemoryStream(bytes), fileName, isSpoiler)
-        { }
-
-        public LocalAttachment(ArraySegment<byte> segment, string fileName, bool isSpoiler = false)
-            : this(new MemoryStream(segment.Array, segment.Offset, segment.Count), fileName, isSpoiler)
-        { }
 
         internal string GetFileName()
             => IsSpoiler
@@ -67,5 +57,20 @@ namespace Disqord
         /// </summary>
         public void Dispose()
             => Stream.Dispose();
+
+        public LocalAttachment Clone()
+            => MemberwiseClone() as LocalAttachment;
+
+        object ICloneable.Clone()
+            => Clone();
+
+        public void Validate()
+        {
+            if (Stream == null)
+                throw new InvalidOperationException("The attachment's stream must be set.");
+
+            if (FileName == null)
+                throw new InvalidOperationException("The attachment's file name must be set.");
+        }
     }
 }
