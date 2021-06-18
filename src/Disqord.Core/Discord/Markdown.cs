ebb48211f1545eaf88f0cc877a0f8f609994bca0
Quahu <quahuu@gmail.com>
2021-06-18 12:17:26 +0200

    Added object overloads to Markdown methods.

--- src/Disqord.Core/Discord/Markdown.cs
+++ src/Disqord.Core/Discord/Markdown.cs
@@ -24,189 +24,93 @@ namespace Disqord
 
         // *text*
         public static string Italics(object value)
-        {
-            if (value == null)
-                throw new ArgumentNullException(nameof(value));
-
-            return Italics(value.ToString().AsSpan());
-        }
+            => Italics((value?.ToString()).AsSpan());
 
         public static string Italics(string text)
-        {
-            if (text == null)
-                throw new ArgumentNullException(nameof(text));
-
-            return Italics(text.AsSpan());
-        }
+            => Italics(text.AsSpan());
 
         public static string Italics(ReadOnlySpan<char> text)
             => string.Concat("*", text, "*");
 
         // **text**
         public static string Bold(object value)
-        {
-            if (value == null)
-                throw new ArgumentNullException(nameof(value));
-
-            return Bold(value.ToString().AsSpan());
-        }
+            => Bold((value?.ToString()).AsSpan());
 
         public static string Bold(string text)
-        {
-            if (text == null)
-                throw new ArgumentNullException(nameof(text));
-
-            return Bold(text.AsSpan());
-        }
+            => Bold(text.AsSpan());
 
         public static string Bold(ReadOnlySpan<char> text)
             => string.Concat("**", text, "**");
 
         // ***text***
         public static string BoldItalics(object value)
-        {
-            if (value == null)
-                throw new ArgumentNullException(nameof(value));
-
-            return BoldItalics(value.ToString().AsSpan());
-        }
+            => BoldItalics((value?.ToString()).AsSpan());
 
         public static string BoldItalics(string text)
-        {
-            if (text == null)
-                throw new ArgumentNullException(nameof(text));
-
-            return BoldItalics(text.AsSpan());
-        }
+            => BoldItalics(text.AsSpan());
 
         public static string BoldItalics(ReadOnlySpan<char> text)
             => string.Concat("***", text, "***");
 
         // __text__
         public static string Underline(object value)
-        {
-            if (value == null)
-                throw new ArgumentNullException(nameof(value));
-
-            return Underline(value.ToString().AsSpan());
-        }
+            => Underline((value?.ToString()).AsSpan());
 
         public static string Underline(string text)
-        {
-            if (text == null)
-                throw new ArgumentNullException(nameof(text));
-
-            return Underline(text.AsSpan());
-        }
+            => Underline(text.AsSpan());
 
         public static string Underline(ReadOnlySpan<char> text)
             => string.Concat("__", text, "__");
 
         // ~~text~~
         public static string Strikethrough(object value)
-        {
-            if (value == null)
-                throw new ArgumentNullException(nameof(value));
-
-            return Strikethrough(value.ToString().AsSpan());
-        }
+            => Strikethrough((value?.ToString()).AsSpan());
 
         public static string Strikethrough(string text)
-        {
-            if (text == null)
-                throw new ArgumentNullException(nameof(text));
-
-            return Strikethrough(text.AsSpan());
-        }
+            => Strikethrough(text.AsSpan());
 
         public static string Strikethrough(ReadOnlySpan<char> text)
             => string.Concat("~~", text, "~~");
 
         // [title](url)
         public static string Link(string title, string url)
-        {
-            if (title == null)
-                throw new ArgumentNullException(nameof(title));
-
-            if (url == null)
-                throw new ArgumentNullException(nameof(url));
-
-            return Link(title.AsSpan(), url.AsSpan());
-        }
+            => Link(title.AsSpan(), url.AsSpan());
 
         public static string Link(ReadOnlySpan<char> title, ReadOnlySpan<char> url)
             => new StringBuilder().Append('[').Append(title).Append("](").Append(url).Append(')').ToString();
 
         // `code`
         public static string Code(object value)
-        {
-            if (value == null)
-                throw new ArgumentNullException(nameof(value));
-
-            return Code(value.ToString().AsSpan());
-        }
+            => Code((value?.ToString()).AsSpan());
 
         public static string Code(string code)
-        {
-            if (code == null)
-                throw new ArgumentNullException(nameof(code));
-
-            return Code(code.AsSpan());
-        }
+            => Code(code.AsSpan());
 
         public static string Code(ReadOnlySpan<char> code)
             => string.Concat("`", code, "`");
 
         // ```\ncode```
         public static string CodeBlock(object value)
-        {
-            if (value == null)
-                throw new ArgumentNullException(nameof(value));
-
-            return CodeBlock(value.ToString().AsSpan());
-        }
+            => CodeBlock((value?.ToString()).AsSpan());
 
         public static string CodeBlock(string code)
-        {
-            if (code == null)
-                throw new ArgumentNullException(nameof(code));
-
-            return CodeBlock(code.AsSpan());
-        }
+            => CodeBlock(code.AsSpan());
 
         public static string CodeBlock(ReadOnlySpan<char> code)
             => string.Concat("```\n", code, "```");
 
         // ```language\ncode```
         public static string CodeBlock(string language, object value)
-        {
-            if (value == null)
-                throw new ArgumentNullException(nameof(value));
-
-            return CodeBlock(language, value.ToString());
-        }
+            => CodeBlock(language, (value?.ToString()));
 
         public static string CodeBlock(string language, string code)
-        {
-            if (language == null)
-                throw new ArgumentNullException(nameof(language));
-
-            if (code == null)
-                throw new ArgumentNullException(nameof(code));
-
-            return CodeBlock(language.AsSpan(), code.AsSpan());
-        }
+            => CodeBlock(language.AsSpan(), code.AsSpan());
 
         public static string CodeBlock(ReadOnlySpan<char> language, ReadOnlySpan<char> code)
             => new StringBuilder().Append("```").Append(language).Append('\n').Append(code).Append("```").ToString();
 
         public static string Escape(string text)
-        {
-            if (text == null)
-                throw new ArgumentNullException(nameof(text));
-
-            return Escape(text.AsSpan());
-        }
+            => Escape(text.AsSpan());
 
         public static string Escape(ReadOnlySpan<char> text)
         {
