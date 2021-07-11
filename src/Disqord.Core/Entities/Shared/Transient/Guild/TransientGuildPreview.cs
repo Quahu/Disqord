58c27e13b869e94252f56eb57202e016c9359de7
Aarav Navani <aarav.navani@outlook.com>
2021-07-11 16:06:54 +0530

    Added missing XML docs

--- src/Disqord.Core/Entities/Shared/Transient/Guild/TransientGuildPreview.cs
+++ src/Disqord.Core/Entities/Shared/Transient/Guild/TransientGuildPreview.cs
@@ -6,20 +6,28 @@ namespace Disqord
 {
     public class TransientGuildPreview : TransientEntity<GuildPreviewJsonModel>, IGuildPreview
     {
+        /// <inheritdoc />
         public Snowflake GuildId { get; }
         
+        /// <inheritdoc />
         public string Name => Model.Name;
 
+        /// <inheritdoc />
         public Snowflake Id => Model.Id;
 
+        /// <inheritdoc />
         public string IconHash => Model.Icon;
 
+        /// <inheritdoc />
         public string SplashHash => Model.Splash;
 
+        /// <inheritdoc />
         public string DiscoverySplashHash => Model.DiscoverySplash.GetValueOrDefault();
 
+        /// <inheritdoc />
         public GuildFeatures Features => new(Model.Features);
         
+        /// <inheritdoc />
         public IReadOnlyDictionary<Snowflake, IGuildEmoji> Emojis => _emojis ??= Model.Emojis.ToReadOnlyDictionary((Client, Id), (x, _) => x.Id.Value, (x, tuple) =>
         {
             var (client, guildId) = tuple;
@@ -27,10 +35,13 @@ namespace Disqord
         });
         private IReadOnlyDictionary<Snowflake, IGuildEmoji> _emojis;
 
+        /// <inheritdoc />
         public int ApproximateMemberCount => Model.ApproximateMemberCount;
 
+        /// <inheritdoc />
         public int ApproximatePresenceCount => Model.ApproximatePresenceCount;
 
+        /// <inheritdoc />
         public string Description => Model.Description;
         
         public TransientGuildPreview(IClient client, Snowflake guildId, GuildPreviewJsonModel model) 
