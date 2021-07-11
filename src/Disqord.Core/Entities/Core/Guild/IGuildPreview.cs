9c2d825457b0f8370e711410c2bc646084d76fd3
Aarav Navani <aarav.navani@outlook.com>
2021-07-11 15:53:55 +0530

    Added guild preview entities

---
+++ src/Disqord.Core/Entities/Core/Guild/IGuildPreview.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace Disqord
+{
+    /// <summary>
+    ///     Represents a guild preview.
+    /// </summary>
+    public interface IGuildPreview : IGuildEntity, INamable, ISnowflakeEntity
+    {
+        /// <summary>
+        ///     Gets the icon image hash of this guild.
+        /// </summary>
+        string IconHash { get; }
+        
+        /// <summary>
+        ///     Gets the splash image hash of this guild.
+        /// </summary>
+        string SplashHash { get; }
+        
+        /// <summary>
+        ///     Gets the discovery image hash of this guild.
+        /// </summary>
+        string DiscoverySplashHash { get; }
+        
+        /// <summary>
+        ///     Gets the features of this guild.
+        /// </summary>
+        GuildFeatures Features { get; }
+        
+        /// <summary>
+        ///     Gets the emojis of the guild.
+        /// </summary>
+        IReadOnlyDictionary<Snowflake, IGuildEmoji> Emojis { get; }
+        
+        /// <summary>
+        ///  Gets the approximate number of members in the guild.
+        /// </summary>
+        int ApproximateMemberCount { get; }
+        
+        /// <summary>
+        ///     Gets the approximate number of presences in this guild.
+        /// </summary>
+        int ApproximatePresenceCount { get; }
+        
+        /// <summary>
+        ///     Gets the description for the guild.
+        ///     Only present if the guild is discoverable.
+        /// </summary>
+        string Description { get; }
+    }
+}
---
+++ src/Disqord.Core/Entities/Shared/Transient/Guild/TransientGuildPreview.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Disqord.Collections;
+using Disqord.Models;
+
+namespace Disqord
+{
+    public class TransientGuildPreview : TransientEntity<GuildPreviewJsonModel>, IGuildPreview
+    {
+        public Snowflake GuildId { get; }
+        
+        public string Name => Model.Name;
+
+        public Snowflake Id => Model.Id;
+
+        public string IconHash => Model.Icon;
+
+        public string SplashHash => Model.Splash;
+
+        public string DiscoverySplashHash => Model.DiscoverySplash.GetValueOrDefault();
+
+        public GuildFeatures Features => new(Model.Features);
+        
+        public IReadOnlyDictionary<Snowflake, IGuildEmoji> Emojis => _emojis ??= Model.Emojis.ToReadOnlyDictionary((Client, Id), (x, _) => x.Id.Value, (x, tuple) =>
+        {
+            var (client, guildId) = tuple;
+            return new TransientGuildEmoji(client, guildId, x) as IGuildEmoji;
+        });
+        private IReadOnlyDictionary<Snowflake, IGuildEmoji> _emojis;
+
+        public int ApproximateMemberCount => Model.ApproximateMemberCount;
+
+        public int ApproximatePresenceCount => Model.ApproximatePresenceCount;
+
+        public string Description => Model.Description;
+        
+        public TransientGuildPreview(IClient client, Snowflake guildId, GuildPreviewJsonModel model) 
+            : base(client, model)
+        {
+            GuildId = guildId;
+        }
+    }
+}
