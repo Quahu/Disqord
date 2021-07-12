f31bc5d1ee5c7338e35d3e9cad05c18b3518bbb7
Aarav Navani <aarav.navani@outlook.com>
2021-07-12 11:13:54 +0530

    Implemented vanity invites

---
+++ src/Disqord.Core/Entities/Shared/Transient/Invite/TransientVanityInvite.cs
@@ -0,0 +1,14 @@
+﻿using Disqord.Models;
+
+namespace Disqord
+{
+    public class TransientVanityInvite : TransientEntity<InviteJsonModel>, IVanityInvite
+    {
+        public string Code => Model.Code;
+        public int Uses => Model.Uses.Value;
+        
+        public TransientVanityInvite(IClient client, InviteJsonModel model) 
+            : base(client, model)
+        { }
+    }
+}
