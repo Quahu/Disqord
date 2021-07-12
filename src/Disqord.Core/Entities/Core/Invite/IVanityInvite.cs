5d05f31342d58aa0dabee0a4b685a99da472bd64
Aarav Navani <aarav.navani@outlook.com>
2021-07-12 13:12:13 +0530

    Implemented vanity invites

---
+++ src/Disqord.Core/Entities/Core/Invite/IVanityInvite.cs
@@ -0,0 +1,18 @@
+﻿namespace Disqord
+{
+    /// <summary>
+    ///     Represents a guild's vanity invite.
+    /// </summary>
+    public interface IVanityInvite 
+    {
+        /// <summary>
+        ///     Gets the code of this vanity invite.
+        /// </summary>
+        string Code { get; }
+
+        /// <summary>
+        ///     Gets the uses of this vanity invite.
+        /// </summary>
+        int Uses { get; }
+    }
+}
---
+++ src/Disqord.Core/Entities/Shared/Transient/Invite/TransientVanityInvite.cs
@@ -0,0 +1,17 @@
+﻿using Disqord.Models;
+
+namespace Disqord
+{
+    public class TransientVanityInvite : TransientEntity<InviteJsonModel>, IVanityInvite
+    {
+        /// <inheritdoc/>
+        public string Code => Model.Code;
+        
+        /// <inheritdoc/>
+        public int Uses => Model.Uses.Value;
+
+        public TransientVanityInvite(IClient client, InviteJsonModel model) 
+            : base(client, model)
+        { }
+    }
+}
