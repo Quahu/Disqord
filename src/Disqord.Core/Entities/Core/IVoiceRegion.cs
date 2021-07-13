3cfb0610adab39daa7d26266c0dbb73c6bc88bef
Daniel Baynton <daniel.baynton@hotmail.com>
2021-07-13 08:04:48 +0100

    Fix more problems

--- src/Disqord.Core/Entities/Core/IVoiceRegion.cs
+++ src/Disqord.Core/Entities/Core/IVoiceRegion.cs
@@ -3,18 +3,18 @@
     public interface IVoiceRegion : IEntity, INamable
     {
         /// <summary>
-        ///     Gets the unique ID for this voice region.
+        ///     Gets the ID of this voice region.
         /// </summary>
         string Id { get; }
 
         /// <summary>
-        ///     Gets whether this is the closest voice region to the client.
+        ///     Gets whether this voice region is the closest to the client.
         /// </summary>
         bool IsOptimal { get; }
         
         /// <summary>
         ///     Gets whether this voice region is deprecated.
         /// </summary>
-        bool IsDepreciated { get; }
+        bool IsDeprecated { get; }
     }
 }
--- src/Disqord.Core/Entities/Shared/Transient/TransientVoiceRegion.cs
+++ src/Disqord.Core/Entities/Shared/Transient/TransientVoiceRegion.cs
@@ -5,16 +5,16 @@ namespace Disqord
     public class TransientVoiceRegion : TransientEntity<VoiceRegionJsonModel>, IVoiceRegion
     {
         /// <inheritdoc/>
-        public string Id => Model.Id;
-        
-        /// <inheritdoc/>
         public string Name => Model.Name;
         
+        /// <inheritdoc/>
+        public string Id => Model.Id;
+
         /// <inheritdoc/>
         public bool IsOptimal => Model.Optimal;
         
         /// <inheritdoc/>
-        public bool IsDepreciated => Model.Deprecated;
+        public bool IsDeprecated => Model.Deprecated;
 
         public TransientVoiceRegion(IClient client, VoiceRegionJsonModel model)
             : base(client, model)
