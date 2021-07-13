c448709191cb65ae19b909af382619596502ccd5
Daniel Baynton <daniel.baynton@hotmail.com>
2021-07-13 22:10:26 +0100

    TransientNewsChannel should inherit from TransientTextChannel

--- src/Disqord.Core/Entities/Shared/Transient/Channels/Guild/TransientNewsChannel.cs
+++ src/Disqord.Core/Entities/Shared/Transient/Channels/Guild/TransientNewsChannel.cs
@@ -3,20 +3,8 @@ using Disqord.Models;
 
 namespace Disqord
 {
-    public class TransientNewsChannel : TransientMessageGuildChannel, INewsChannel
+    public class TransientNewsChannel : TransientTextChannel, INewsChannel
     {
-        public string Topic => Model.Topic.Value;
-
-        public bool IsNsfw => Model.Nsfw.Value;
-
-        public bool IsNews => Model.Type == ChannelType.News;
-
-        public TimeSpan DefaultAutomaticArchiveDuration => TimeSpan.FromMinutes(Model.DefaultAutoArchiveDuration.Value);
-
-        public string Mention => Disqord.Mention.TextChannel(this);
-
-        public string Tag => $"#{Name}";
-
         public TransientNewsChannel(IClient client, ChannelJsonModel model)
             : base(client, model)
         { }
