1c1730c1f2c975607ed1670bd352146c388cdc15
Quahu <quahuu@gmail.com>
2021-07-20 09:11:49 +0200

    Lowered the default degree of parallelism of the command queue to 3.

--- src/Disqord.Bot/Queue/Default/DefaultCommandQueueConfiguration.cs
+++ src/Disqord.Bot/Queue/Default/DefaultCommandQueueConfiguration.cs
@@ -6,9 +6,10 @@ namespace Disqord.Bot
     {
         /// <summary>
         ///     Gets or sets the degree of parallelism for the queue,
-        ///     i.e. the amount of parallel command executions per-bucket.
+        ///     i.e. the amount of parallel command executions per-bucket (per-guild).
+        ///     Defaults to <c>3</c>.
         /// </summary>
         [Range(1, int.MaxValue)]
-        public virtual int DegreeOfParallelism { get; set; } = 5;
+        public virtual int DegreeOfParallelism { get; set; } = 3;
     }
 }
