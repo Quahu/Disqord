0319b4c9f546110e47b114735a49bd44a44bc7de
Quahu <quahuu@gmail.com>
2021-07-22 01:00:43 +0200

    Added DeleteAfter() for response results.

---
+++ src/Disqord.Bot/Commands/Results/DiscordTemporaryResponseCommandResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Disqord.Rest;
+
+namespace Disqord.Bot
+{
+    public class DiscordTemporaryResponseCommandResult : DiscordCommandResult
+    {
+        public DiscordResponseCommandResult Result { get; protected set; }
+
+        public TimeSpan Delay { get; protected set; }
+
+        public DiscordTemporaryResponseCommandResult(DiscordResponseCommandResult result, TimeSpan delay)
+            : base(result.Context)
+        {
+            Result = result;
+            Delay = delay;
+        }
+
+        public override async Task ExecuteAsync()
+        {
+            var message = await Result.ExecuteAsync().ConfigureAwait(false);
+            await Task.Delay(Delay).ConfigureAwait(false);
+            await message.DeleteAsync().ConfigureAwait(false);
+        }
+    }
+}
---
+++ src/Disqord.Bot/Commands/Results/Extensions/DiscordResponseCommandResultExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Disqord.Bot
+{
+    public static class DiscordResponseCommandResultExtensions
+    {
+        /// <summary>
+        ///     Wraps the <see cref="DiscordResponseCommandResult"/> in a new result which will
+        ///     delete the sent response after the specified delay.
+        /// </summary>
+        /// <param name="result"> The result to wrap. </param>
+        /// <param name="delay"> The delay after which the response should be deleted. </param>
+        /// <returns>
+        ///     A <see cref="DiscordTemporaryResponseCommandResult"/>.
+        /// </returns>
+        public static DiscordTemporaryResponseCommandResult DeleteAfter(this DiscordResponseCommandResult result, TimeSpan delay)
+            => new(result, delay);
+    }
+}
