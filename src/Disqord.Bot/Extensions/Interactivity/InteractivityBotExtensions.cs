1bd32d906b5da459ef73df9d7facc9e3a53390be
Quahu <quahuu@gmail.com>
2021-07-22 01:08:53 +0200

    Removed the redundant checks from the interactivity predicates.

--- src/Disqord.Bot/Extensions/Interactivity/InteractivityBotExtensions.cs
+++ src/Disqord.Bot/Extensions/Interactivity/InteractivityBotExtensions.cs
@@ -13,7 +13,7 @@ namespace Disqord.Extensions.Interactivity
         public static Task<MessageReceivedEventArgs> WaitForMessageAsync(this DiscordCommandContext context, Predicate<MessageReceivedEventArgs> predicate = null, TimeSpan timeout = default, CancellationToken cancellationToken = default)
         {
             var extension = context.Bot.GetInteractivity();
-            return extension.WaitForMessageAsync(context.ChannelId, predicate + (e => e.ChannelId == context.ChannelId && e.Message.Author.Id == context.Author.Id), timeout, cancellationToken);
+            return extension.WaitForMessageAsync(context.ChannelId, predicate + (e => e.Message.Author.Id == context.Author.Id), timeout, cancellationToken);
         }
     }
 }
