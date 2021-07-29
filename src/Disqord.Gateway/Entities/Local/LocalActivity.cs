641487cccc4f9ffab1ab02953c0c663a89e4b2a5
Quahu <quahuu@gmail.com>
2021-07-30 01:01:25 +0200

    Added static ctor utilities to LocalActivity.

--- src/Disqord.Gateway/Entities/Local/LocalActivity.cs
+++ src/Disqord.Gateway/Entities/Local/LocalActivity.cs
@@ -2,6 +2,21 @@
 {
     public class LocalActivity
     {
+        public static LocalActivity Playing(string name)
+            => new(name, ActivityType.Playing);
+
+        public static LocalActivity Streaming(string name, string url)
+            => new(name, url);
+
+        public static LocalActivity Listening(string name)
+            => new(name, ActivityType.Listening);
+
+        public static LocalActivity Watching(string name)
+            => new(name, ActivityType.Watching);
+
+        public static LocalActivity Competing(string name)
+            => new(name, ActivityType.Competing);
+
         public string Name { get; }
 
         public string Url { get; }
