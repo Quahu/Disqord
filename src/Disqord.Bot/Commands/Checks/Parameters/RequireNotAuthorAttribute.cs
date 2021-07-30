85dc046d912aca8fec7cd25dbbbf840edef5ca8b
Quahu <quahuu@gmail.com>
2021-07-30 04:59:29 +0200

    Added RequireNotAuthorAttribute.

---
+++ src/Disqord.Bot/Commands/Checks/Parameters/RequireNotAuthorAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Qmmands;
+
+namespace Disqord.Bot
+{
+    /// <summary>
+    ///     Specifies that the <see cref="IUser"/> parameter must not be the author.
+    /// </summary>
+    public class RequireNotAuthorAttribute : DiscordParameterCheckAttribute
+    {
+        public override bool CheckType(Type type)
+            => typeof(IUser).IsAssignableFrom(type);
+
+        public override ValueTask<CheckResult> CheckAsync(object argument, DiscordCommandContext context)
+        {
+            if (argument is IUser user && user.Id != context.Author.Id)
+                return Success();
+
+            return Failure("The provided argument must be another user.");
+        }
+    }
+}
