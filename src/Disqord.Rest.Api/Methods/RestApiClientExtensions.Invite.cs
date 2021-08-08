9bbb11872630fc898b8ffbc25671a7e1aa2d0487
Zane Pereira <zanedpereira2@gmail.com>
2021-08-08 11:46:27 +0530

    Opps

--- src/Disqord.Rest.Api/Methods/RestApiClientExtensions.Invite.cs
+++ src/Disqord.Rest.Api/Methods/RestApiClientExtensions.Invite.cs
@@ -1,13 +1,20 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Disqord.Models;
 
 namespace Disqord.Rest.Api
 {
     public static partial class RestApiClientExtensions
     {
-        public static Task<InviteJsonModel> FetchInviteAsync(this IRestApiClient client, string code, IRestRequestOptions options = null)
+        public static Task<InviteJsonModel> FetchInviteAsync(this IRestApiClient client, string code, bool withCounts = false, bool withExpiration = false, IRestRequestOptions options = null)
         {
-            var route = Format(Route.Invite.GetInvite, code);
+            var queryParameters = new Dictionary<string, object>(2)
+            {
+                ["with_counts"] = withCounts,
+                ["with_expiration"] = withExpiration
+            };
+
+            var route = Format(Route.Invite.GetInvite, queryParameters, code);
             return client.ExecuteAsync<InviteJsonModel>(route, null, options);
         }
 
