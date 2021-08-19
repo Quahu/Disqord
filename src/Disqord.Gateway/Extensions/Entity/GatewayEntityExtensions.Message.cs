a4aad7c5d6b3ab7f148fb3abb592094017507619
Quahu <quahuu@gmail.com>
2021-08-19 18:09:14 +0200

    Changed IGatewayMessage.GetChannel()'s return type.

--- src/Disqord.Gateway/Extensions/Entity/GatewayEntityExtensions.Message.cs
+++ src/Disqord.Gateway/Extensions/Entity/GatewayEntityExtensions.Message.cs
@@ -3,20 +3,20 @@
     public static partial class GatewayEntityExtensions
     {
         /// <summary>
-        ///     Gets the <see cref="CachedTextChannel"/> for the specified gateway message.
+        ///     Gets the <see cref="CachedMessageGuildChannel"/> for the specified gateway message.
         ///     Returns <see langword="null"/> if the channel is not cached or the message was sent in a private channel.
         /// </summary>
         /// <param name="message"> The message to get the channel for. </param>
         /// <returns>
         ///     The cached channel of this message.
         /// </returns>
-        public static CachedTextChannel GetChannel(this IGatewayMessage message)
+        public static CachedMessageGuildChannel GetChannel(this IGatewayMessage message)
         {
             if (message.GuildId == null)
                 return null;
 
             var client = message.GetGatewayClient();
-            return client.GetChannel(message.GuildId.Value, message.ChannelId) as CachedTextChannel;
+            return client.GetChannel(message.GuildId.Value, message.ChannelId) as CachedMessageGuildChannel;
         }
 
         /// <summary>
