147942a0ec63da38e9ae49703f6fed91450195e1
Quahu <quahuu@gmail.com>
2021-09-01 17:44:05 +0200

    Added initial test docs.

---
+++ assets/jquery.js
@@ -0,0 +1,11 @@
+$("h1, h2, h3").addClass("border-bottom pb-1");
+
+$(".container > :header").addClass("header-margin");
+
+$("code").not("pre > code").addClass("hljs");
+
+$(".alert").find("a").addClass("alert-link");
+
+$(":header.header-margin").not("h1").addClass("anchored");
+
+$(":header").css("color", "white");
