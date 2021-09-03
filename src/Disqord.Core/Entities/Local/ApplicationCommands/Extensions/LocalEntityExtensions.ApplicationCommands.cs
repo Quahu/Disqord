feef985395c7b3dde137b265b1d13deb06ba5937
Neuheit <38368299+Neuheit@users.noreply.github.com>
2021-09-03 11:20:58 -0400

    Finish up implementing the application command rest endpoints.

---
+++ src/Disqord.Core/Entities/Local/ApplicationCommands/Extensions/LocalEntityExtensions.ApplicationCommands.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Disqord.Models;
+using Disqord.Serialization.Json;
+
+namespace Disqord
+{
+    public static partial class LocalEntityExtensions
+    {
+        public static ApplicationCommandOptionJsonModel ToModel(this LocalApplicationCommandOption option, IJsonSerializer serializer)
+            => option == null ? null : new ApplicationCommandOptionJsonModel
+            {
+                Type = option.Type,
+                Name = option.Name,
+                Description = option.Description,
+                Required = option.Required,
+                Choices = option.Choices.Select(x => x.ToModel(serializer)).ToArray(),
+                Options = option.Options.Select(x => x.ToModel(serializer)).ToArray()
+            };
+
+        public static ApplicationCommandOptionChoiceJsonModel ToModel(this LocalApplicationCommandOptionChoice choice, IJsonSerializer serializer)
+            => choice == null ? null : new ApplicationCommandOptionChoiceJsonModel
+            {
+                Name = choice.Name,
+                Value = (IJsonValue)serializer.GetJsonNode(choice.Value)
+            };
+    }
+}
---
+++ src/Disqord.Rest/ActionProperties/Create/CreateApplicationCommandActionProperties.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disqord.Rest
+{
+    public sealed class CreateApplicationCommandActionProperties
+    {
+        public Optional<IEnumerable<LocalApplicationCommandOption>> Options { internal get; set; }
+
+        public Optional<bool> IsEnabledByDefault { internal get; set; }
+
+        public Optional<ApplicationCommandType> Type { internal get; set; }
+    }
+}
