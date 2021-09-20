cd5154a6be125946ae054cef59543ad11ac0fa1c
Quahu <quahuu@gmail.com>
2021-09-20 08:51:44 +0200

    Added extensions for the default REST request options.

---
+++ src/Disqord.Rest.Api/Default/DefaultRestRequestOptionsExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using Disqord.Rest.Api;
+using Disqord.Rest.Api.Default;
+
+namespace Disqord.Rest
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static class DefaultRestRequestOptionsExtensions
+    {
+        public static TOptions WithRequestAction<TOptions>(this TOptions options, Action<IRestRequest> action)
+            where TOptions : DefaultRestRequestOptions
+        {
+            options.RequestAction = action;
+            return options;
+        }
+
+        public static TOptions WithHeadersAction<TOptions>(this TOptions options, Action<DefaultRestResponseHeaders> action)
+            where TOptions : DefaultRestRequestOptions
+        {
+            options.HeadersAction = action;
+            return options;
+        }
+    }
+}
