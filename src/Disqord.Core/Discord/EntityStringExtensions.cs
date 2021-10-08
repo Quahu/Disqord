a0d60d3ce57aaad0f1c88bcbeb307ba690db43a7
Quahu <quahuu@gmail.com>
2021-10-08 03:29:04 +0200

    Added a common class for ToString() entity representations.

---
+++ src/Disqord.Core/Discord/EntityStringExtensions.cs
@@ -0,0 +1,49 @@
+namespace Disqord
+{
+    internal static class EntityStringExtensions
+    {
+        public static string GetString(this IEntity entity)
+        {
+            var namable = entity as INamableEntity;
+            var identifiable = entity as IIdentifiableEntity;
+            if (namable != null && identifiable != null)
+                return $"'{namable.Name}' (ID: {identifiable.Id})";
+
+            if (namable != null)
+                return $"'{namable.Name}'";
+
+            return $"ID: {identifiable.Id}";
+        }
+
+        public static string GetString(this IUser user)
+            => user.Tag;
+
+        public static string GetString(this IPartialSticker sticker)
+            => $"'{sticker.Name}' ({sticker.Id}, {sticker.FormatType})";
+
+        public static string GetString(this IMessageReference reference)
+            => $"{reference.GuildId?.ToString() ?? "<no guild ID>"}/{reference.ChannelId}/{reference.MessageId?.ToString() ?? "<no message ID>"}";
+
+        public static string GetString(this IMessageReaction reaction)
+            => $"'{reaction.Emoji}': {reaction.Count} (own reaction: {reaction.HasOwnReaction})";
+
+        public static string GetString(this IBan ban)
+            => $"{ban.User.Tag}: {ban.Reason}";
+
+        public static string GetString(this IVoiceRegion region)
+            => $"'{region.Name}' ({region.Id})";
+
+        public static string GetString(this IEmoji emoji)
+        {
+            if (emoji is ICustomEmoji customEmoji)
+            {
+                if (customEmoji.IsAnimated)
+                    return $"<a:{customEmoji.Name ?? "_"}:{customEmoji.Id}>";
+
+                return $"<:{customEmoji.Name ?? "_"}:{customEmoji.Id}>";
+            }
+
+            return emoji.Name;
+        }
+    }
+}
