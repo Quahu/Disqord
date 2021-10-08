5806b8fe4a48932368fc16183ce448ccef1967c8
Quahu <quahuu@gmail.com>
2021-10-08 02:28:36 +0200

    Added IBan.DeleteAsync().

---
+++ src/Disqord.Rest/Extensions/Entity/RestEntityExtensions.Ban.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Disqord.Rest
+{
+    public static partial class RestEntityExtensions
+    {
+        public static Task DeleteAsync(this IBan ban,
+            IRestRequestOptions options = null, CancellationToken cancellationToken = default)
+        {
+            var client = ban.GetRestClient();
+            return client.DeleteBanAsync(ban.GuildId, ban.User.Id, options, cancellationToken);
+        }
+    }
+}
