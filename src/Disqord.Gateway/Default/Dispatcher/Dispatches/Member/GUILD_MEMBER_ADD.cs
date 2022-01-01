a013b3c0229c4e0ef6343a84d10e375d8efdbbb4
Quahu <quahuu@gmail.com>
2022-01-01 23:36:54 +0100

    Implemented duplicate GUILD_MEMBER_ADD/REMOVE prevention.

--- src/Disqord.Gateway/Default/Dispatcher/Dispatches/Member/GUILD_MEMBER_ADD.cs
+++ src/Disqord.Gateway/Default/Dispatcher/Dispatches/Member/GUILD_MEMBER_ADD.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Disqord.Gateway.Api;
 using Disqord.Gateway.Api.Models;
 
@@ -6,10 +7,46 @@ namespace Disqord.Gateway.Default.Dispatcher
 {
     public class GuildMemberAddHandler : Handler<GuildMemberAddJsonModel, MemberJoinedEventArgs>
     {
+        private GuildMemberRemoveHandler _guildMemberRemoveHandler;
+
+        // GuildId -> MemberId
+        private Dictionary<Snowflake, Snowflake> _lastMemberIds;
+
+        public override void Bind(DefaultGatewayDispatcher value)
+        {
+            _guildMemberRemoveHandler = value[GatewayDispatchNames.GuildMemberRemove] as GuildMemberRemoveHandler;
+            if (_guildMemberRemoveHandler != null)
+                _lastMemberIds = new();
+
+            base.Bind(value);
+        }
+
+        public void OnMemberRemove(Snowflake guildId, Snowflake memberId)
+        {
+            if (_lastMemberIds.TryGetValue(guildId, out var lastMemberId) && lastMemberId == memberId)
+                _lastMemberIds.Remove(guildId);
+        }
+
         public override ValueTask<MemberJoinedEventArgs> HandleDispatchAsync(IGatewayApiClient shard, GuildMemberAddJsonModel model)
         {
             var guild = Client.GetGuild(model.GuildId);
-            guild?.Update(model); // Increments the member count.
+            if (_lastMemberIds != null)
+            {
+                if (_lastMemberIds.TryGetValue(model.GuildId, out var lastMemberId) && lastMemberId == model.User.Value.Id)
+                {
+                    // If the event is a duplicate, we don't handle it nor trigger event handlers.
+                    return default;
+                }
+
+                // Increments the guild member count if the received event is not a duplicate.
+                guild?.Update(model);
+
+                // Stores the user ID to check in the next received dispatch.
+                _lastMemberIds[model.GuildId] = model.User.Value.Id;
+
+                // Notifies GuildMemberRemoveHandler that the user joined, so it can clear its own duplicate cache.
+                _guildMemberRemoveHandler.OnMemberAdd(model.GuildId, model.User.Value.Id);
+            }
 
             var member = Dispatcher.GetOrAddMemberTransient(model.GuildId, model);
             var e = new MemberJoinedEventArgs(guild, member);
