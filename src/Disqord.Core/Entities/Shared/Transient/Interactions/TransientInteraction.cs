936700b40dedfbaeeb039a5145edd15a129780da
Quahu <quahuu@gmail.com>
2022-02-10 06:23:28 +0100

    Pushed the TransientInteraction.Create() change.

--- src/Disqord.Core/Entities/Shared/Transient/Interactions/TransientInteraction.cs
+++ src/Disqord.Core/Entities/Shared/Transient/Interactions/TransientInteraction.cs
@@ -40,13 +40,14 @@ namespace Disqord.Interaction
         public static IInteraction Create(IClient client, InteractionJsonModel model)
             => model.Type switch
             {
-                InteractionType.ApplicationCommand or InteractionType.ApplicationCommandAutoComplete => model.Data.Value.Type.Value switch
+                InteractionType.ApplicationCommand => model.Data.Value.Type.Value switch
                 {
                     ApplicationCommandType.Slash => new TransientSlashCommandInteraction(client, model),
                     ApplicationCommandType.User or ApplicationCommandType.Message => new TransientContextMenuInteraction(client, model),
                     _ => new TransientApplicationCommandInteraction(client, model)
                 },
                 InteractionType.MessageComponent => new TransientComponentInteraction(client, model),
+                InteractionType.ApplicationCommandAutoComplete => new TransientSlashCommandAutoCompleteInteraction(client, model),
                 _ => new TransientInteraction(client, model)
             };
     }
