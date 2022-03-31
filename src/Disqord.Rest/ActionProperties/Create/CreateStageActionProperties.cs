942e7a94d498ee09f80efb7591fb9dcac81059c2
Zane Pereira <zanedpereira2@gmail.com>
2022-03-31 08:06:38 +0530

    Updated create stage endpoint, events audit log. (#85)
    * Added stage start notification
    
    * Added missing guild event auditlog key

--- src/Disqord.Rest/ActionProperties/Create/CreateStageActionProperties.cs
+++ src/Disqord.Rest/ActionProperties/Create/CreateStageActionProperties.cs
@@ -3,5 +3,7 @@ namespace Disqord
     public sealed class CreateStageActionProperties
     {
         public Optional<PrivacyLevel> PrivacyLevel { internal get; set; }
+
+        public Optional<bool> NotifyEveryone { internal get; set; }
     }
 }
