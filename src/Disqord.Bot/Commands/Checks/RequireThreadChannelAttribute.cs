a6a4b8bc9331fa6acc5d8e8ee9621da550b4e34f
MindHardt <71509337+MindHardt@users.noreply.github.com>
2022-05-17 20:47:30 +0500

    Added RequireThreadChannelAttribute and RequireReferencedMessageAttribute.

---
+++ src/Disqord.Bot/Commands/Checks/RequireThreadChannelAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Qmmands;
+
+namespace Disqord.Bot
+{
+    /// <summary>
+    ///     Specifies that the module or command can only be executed in thread channel.
+    /// </summary>
+    public class RequireThreadChannelAttribute : DiscordGuildCheckAttribute
+    {
+        public RequireThreadChannelAttribute()
+        { }
+
+        public override ValueTask<CheckResult> CheckAsync(DiscordGuildCommandContext context)
+        {
+            if (context.Channel is IThreadChannel)
+                return Success();
+
+            return Failure($"This can only be executed in thread channel.");
+        }
+    }
+}
