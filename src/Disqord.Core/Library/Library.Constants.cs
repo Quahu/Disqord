16097bb179d1ac39e1eca06b048b4bad4b58d53a
Quahu <quahuu@gmail.com>
2022-09-05 01:35:07 +0200

    Bumped the API versions to 10.

--- src/Disqord.Core/Library/Library.Constants.cs
+++ src/Disqord.Core/Library/Library.Constants.cs
@@ -4,9 +4,9 @@ public static partial class Library
 {
     public static class Constants
     {
-        public const int GatewayApiVersion = 9;
+        public const int GatewayApiVersion = 10;
 
-        public const int RestApiVersion = 9;
+        public const int RestApiVersion = 10;
 
         public const int VoiceVersion = 4;
     }
