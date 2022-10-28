f83a9df3c22b93bab1466d82d4e3bd6b1e32ad61
Quahu <quahuu@gmail.com>
2022-10-28 14:50:42 +0200

    Updated NitroType.

--- src/Disqord.Core/Enums/NitroType.cs
+++ src/Disqord.Core/Enums/NitroType.cs
@@ -6,12 +6,22 @@
 public enum NitroType
 {
     /// <summary>
+    ///     The user does not have Nitro.
+    /// </summary>
+    None = 0,
+
+    /// <summary>
     ///     The user has Nitro Classic.
     /// </summary>
-    Classic,
+    Classic = 1,
 
     /// <summary>
     ///     The user has normal Nitro.
     /// </summary>
-    Normal
+    Normal = 2,
+
+    /// <summary>
+    ///     The user has Nitro Basic.
+    /// </summary>
+    Basic = 3
 }
