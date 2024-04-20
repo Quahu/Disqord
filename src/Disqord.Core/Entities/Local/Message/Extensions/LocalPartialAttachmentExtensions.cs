0e80a8c22c33ba326ada2f5f59ceaa685d34fc92
Quahu <quahuu@gmail.com>
2024-04-20 16:58:27 +0200

    Moved WithId and WithDescription up to LocalPartialAttachment.

---
+++ src/Disqord.Core/Entities/Local/Message/Extensions/LocalPartialAttachmentExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace Disqord;
+
+/// <summary>
+///     Defines <see cref="LocalPartialAttachment"/> extensions.
+/// </summary>
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static class LocalPartialAttachmentExtensions
+{
+    public static TAttachment WithId<TAttachment>(this TAttachment attachment, Snowflake id)
+        where TAttachment : LocalPartialAttachment
+    {
+        attachment.Id = id;
+        return attachment;
+    }
+
+    public static TAttachment WithDescription<TAttachment>(this TAttachment attachment, string description)
+        where TAttachment : LocalPartialAttachment
+    {
+        attachment.Description = description;
+        return attachment;
+    }
+}
