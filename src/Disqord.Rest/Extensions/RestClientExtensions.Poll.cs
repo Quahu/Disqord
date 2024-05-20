d0fc6101303285335ff070fab7ed2646aa4e4cdf
Quahu <quahuu@gmail.com>
2024-05-20 23:42:23 +0200

    Added entity extensions. Added poll prefix to method name

--- src/Disqord.Rest/Extensions/RestClientExtensions.Poll.cs
+++ src/Disqord.Rest/Extensions/RestClientExtensions.Poll.cs
@@ -10,7 +10,7 @@ namespace Disqord.Rest;
 
 public static partial class RestClientExtensions
 {
-    public static IPagedEnumerable<IUser> EnumerateAnswerVoters(this IRestClient client,
+    public static IPagedEnumerable<IUser> EnumeratePollAnswerVoters(this IRestClient client,
         Snowflake channelId, Snowflake messageId, int answerId, int limit, Snowflake? startFromId = null,
         IRestRequestOptions? options = null)
     {
@@ -19,11 +19,11 @@ public static partial class RestClientExtensions
         return PagedEnumerable.Create((state, cancellationToken) =>
         {
             var (client, channelId, messageId, answerId, limit, startFromId, options) = state;
-            return new FetchAnswerVotersPagedEnumerator(client, channelId, messageId, answerId, limit, startFromId, options, cancellationToken);
+            return new FetchPollAnswerVotersPagedEnumerator(client, channelId, messageId, answerId, limit, startFromId, options, cancellationToken);
         }, (client, channelId, messageId, answerId, limit, startFromId, options));
     }
 
-    public static Task<IReadOnlyList<IUser>> FetchAnswerVotersAsync(this IRestClient client,
+    public static Task<IReadOnlyList<IUser>> FetchPollAnswerVotersAsync(this IRestClient client,
         Snowflake channelId, Snowflake messageId, int answerId, int limit = Discord.Limits.Rest.FetchPollAnswerVotersPageSize, Snowflake? startFromId = null,
         IRestRequestOptions? options = null, CancellationToken cancellationToken = default)
     {
@@ -31,13 +31,13 @@ public static partial class RestClientExtensions
             return Task.FromResult(ReadOnlyList<IUser>.Empty);
 
         if (limit <= Discord.Limits.Rest.FetchPollAnswerVotersPageSize)
-            return client.InternalFetchAnswerVotersAsync(channelId, messageId, answerId, limit, startFromId, options, cancellationToken);
+            return client.InternalFetchPollAnswerVotersAsync(channelId, messageId, answerId, limit, startFromId, options, cancellationToken);
 
-        var enumerable = client.EnumerateAnswerVoters(channelId, messageId, answerId, limit, startFromId, options);
+        var enumerable = client.EnumeratePollAnswerVoters(channelId, messageId, answerId, limit, startFromId, options);
         return enumerable.FlattenAsync(cancellationToken);
     }
 
-    internal static async Task<IReadOnlyList<IUser>> InternalFetchAnswerVotersAsync(this IRestClient client,
+    internal static async Task<IReadOnlyList<IUser>> InternalFetchPollAnswerVotersAsync(this IRestClient client,
         Snowflake channelId, Snowflake messageId, int answerId, int limit, Snowflake? startFromId,
         IRestRequestOptions? options, CancellationToken cancellationToken)
     {
--- src/Disqord.Rest/Requests/Pagination/Implementation/FetchPollAnswerVotersPagedEnumerator.cs
+++ src/Disqord.Rest/Requests/Pagination/Implementation/FetchPollAnswerVotersPagedEnumerator.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 
 namespace Disqord.Rest;
 
-public class FetchAnswerVotersPagedEnumerator : PagedEnumerator<IUser>
+public class FetchPollAnswerVotersPagedEnumerator : PagedEnumerator<IUser>
 {
     public override int PageSize => Discord.Limits.Rest.FetchPollAnswerVotersPageSize;
 
@@ -13,7 +13,7 @@ public class FetchAnswerVotersPagedEnumerator : PagedEnumerator<IUser>
     private readonly int _answerId;
     private readonly Snowflake? _startFromId;
 
-    public FetchAnswerVotersPagedEnumerator(
+    public FetchPollAnswerVotersPagedEnumerator(
         IRestClient client,
         Snowflake channelId, Snowflake messageId, int answerId, int limit, Snowflake? startFromId,
         IRestRequestOptions? options,
@@ -33,6 +33,6 @@ public class FetchAnswerVotersPagedEnumerator : PagedEnumerator<IUser>
         if (previousPage != null && previousPage.Count > 0)
             startFromId = previousPage[^1].Id;
 
-        return Client.InternalFetchAnswerVotersAsync(_channelId, _messageId, _answerId, NextPageSize, startFromId, options, cancellationToken);
+        return Client.InternalFetchPollAnswerVotersAsync(_channelId, _messageId, _answerId, NextPageSize, startFromId, options, cancellationToken);
     }
 }
