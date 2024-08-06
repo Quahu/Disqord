4870219b53647a23d42a289a5f64be3a5eed16a6
Daniel Jarski <30324210+QuantumToasted@users.noreply.github.com>
2024-08-05 23:13:38 -0500

    IApplicationEntity should inherit from IEntity

--- src/Disqord.Core/Entities/Core/IApplicationEntity.cs
+++ src/Disqord.Core/Entities/Core/IApplicationEntity.cs
@@ -3,7 +3,7 @@
 /// <summary>
 ///     Represents a Discord entity that exists within an application.
 /// </summary>
-public interface IApplicationEntity
+public interface IApplicationEntity : IEntity
 {
     /// <summary>
     ///     Gets the ID of the application this entity is tied to.
