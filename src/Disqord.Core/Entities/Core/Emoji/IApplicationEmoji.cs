93d2ee45e849bf8846a7d18cdb51bd435fa590a9
Daniel Jarski <30324210+QuantumToasted@users.noreply.github.com>
2024-08-05 23:27:20 -0500

    Abstracted IGuildEmoji and IApplicationEmoji -> IOwnedEmoji
    IOwnedEmoji as a name was chosen since it represents an emoji that is (potentially) *owned* by a guild or application. Alternatives considered included IDiscordEmoji, but that seemed way too vague and not very descriptive of its common properties.
    
    Also, adjusted XML documentation to compensate for these changes.

--- src/Disqord.Core/Entities/Core/Emoji/IApplicationEmoji.cs
+++ src/Disqord.Core/Entities/Core/Emoji/IApplicationEmoji.cs
@@ -5,20 +5,5 @@ namespace Disqord;
 /// <summary>
 ///     Represents a custom emoji (e.g. <c>&lt;:professor:667582610431803437&gt;</c>) retrieved from an application.
 /// </summary>
-public interface IApplicationEmoji : ICustomEmoji, IApplicationEntity, IClientEntity, INamableEntity, IJsonUpdatable<EmojiJsonModel>
-{
-    /// <summary>
-    ///     Gets the user that created this emoji.
-    /// </summary>
-    IUser? Creator { get; }
-
-    /// <summary>
-    ///     Gets whether this emoji requires colons in chat.
-    /// </summary>
-    bool RequiresColons { get; }
-
-    /// <summary>
-    ///     Gets whether this emoji is managed by an integration.
-    /// </summary>
-    bool IsManaged { get; }
-}
+public interface IApplicationEmoji : IOwnedEmoji, IApplicationEntity, IClientEntity, INamableEntity, IJsonUpdatable<EmojiJsonModel>
+{ }
--- src/Disqord.Core/Entities/Core/Emoji/IGuildEmoji.cs
+++ src/Disqord.Core/Entities/Core/Emoji/IGuildEmoji.cs
@@ -6,27 +6,12 @@ namespace Disqord;
 /// <summary>
 ///     Represents a custom emoji (e.g. <c>&lt;:professor:667582610431803437&gt;</c>) retrieved from a known guild.
 /// </summary>
-public interface IGuildEmoji : ICustomEmoji, IGuildEntity, IClientEntity, INamableEntity, IJsonUpdatable<EmojiJsonModel>
+public interface IGuildEmoji : IOwnedEmoji, IGuildEntity, IClientEntity, INamableEntity, IJsonUpdatable<EmojiJsonModel>
 {
     /// <summary>
     ///     Gets the role IDs that can use this emoji.
     /// </summary>
     IReadOnlyList<Snowflake> RoleIds { get; }
-
-    /// <summary>
-    ///     Gets the user that created this emoji.
-    /// </summary>
-    IUser? Creator { get; }
-
-    /// <summary>
-    ///     Gets whether this emoji requires colons in chat.
-    /// </summary>
-    bool RequiresColons { get; }
-
-    /// <summary>
-    ///     Gets whether this emoji is managed by an integration.
-    /// </summary>
-    bool IsManaged { get; }
 
     /// <summary>
     ///     Gets whether this emoji is available.
